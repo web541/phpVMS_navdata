b115bdf303e0052ba564351a63bb3f382925c5c3
web541 <web541php@gmail.com>
2016-01-20 14:20:20 +1100

    Updated to enable local install

--- db.php
+++ db.php
@@ -1,7 +1,14 @@
 <?php
 
-error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
+// Have Not converted every file to MySqli though might be an option later on
 
+# Stop Error Reporting When Running Script
+ini_set('error_reporting', E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
+
+# Mysqli Connection for ints file
+$connection = mysqli_connect('localhost','db username goes here','db password goes here','db server name goes here');
+
+# Mysql Connection for everything else
 $db = mysql_connect('localhost', 'db username goes here', 'db password goes here');
 mysql_select_db('db server name goes here');
 
